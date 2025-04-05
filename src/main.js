0aa41dee592b10618bd8cd4f573a425d9cf6566c
Plamen Petrov <biznetmen@gmail.com>
2025-04-05 12:17:03 +0300

    Using Nested Routes, Named Routes & Location Objects

--- src/main.js
+++ src/main.js
@@ -11,9 +11,16 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+    {
+      name: 'teams',
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        { name: 'team-members', path: ':teamId', component: TeamMembers, props: true } // /teams/t1
+      ]
+    }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
+
     { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
